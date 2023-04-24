a1049a210037acafee52c6c065ac374611d71810
Rakesh kundan <rakeshkundankps@gmail.com>
2023-04-24 17:34:56 +0530

    menu

--- public/javascript.js
+++ public/javascript.js
@@ -5,7 +5,7 @@ let path=document.getElementById("Menu").addEventListener("click",()=>{
 
   });
   document.getElementsByClassName("body")[0].addEventListener("click",()=>{
-    document.getElementById("img-div").classList.toggle("invisible");
-    document.getElementById("Menu").classList.toggle("activate");
-    document.getElementById("Menu").classList.toggle("deactivate");
+    document.getElementById("img-div").classList.add("invisible");
+    document.getElementById("Menu").classList.remove("activate");
+    document.getElementById("Menu").classList.add("deactivate");
   });
